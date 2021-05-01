95c35fc3c10a3acf1aa18e65b6e0fe8a72a7e209
shubhamnishad <Shubham.nishad007@gmail.com>
2021-05-01 15:01:40 +0530

    Updated

--- src/Screens/Capture.js
+++ src/Screens/Capture.js
@@ -30,13 +30,7 @@ export default function Capture(props) {
             }}
             ref={ref}
             type={RNCamera.Constants.Type.back}
-            captureAudio={false}
-            androidCameraPermissionOptions={{
-              title: 'Permission to use camera',
-              message: 'We need your permission to use your camera',
-              buttonPositive: 'Ok',
-              buttonNegative: 'Cancel',
-            }}>
+            captureAudio={false}>
             <View
               style={{
                 height: 100,
